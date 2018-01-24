7592e0543fab8ac3acb1f0edbda864f027363827
Shen, Xiao <Xiao.Shen@capco.com>
2018-01-24 13:16:18 -0500

    update filter to reflect complete list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -25,12 +25,8 @@ export class AppComponent {
       .set('Access-Control-Allow-Origin', 'true');
 
 
-    // const httpOptions = {
-    //   headers: new HttpHeaders({ 'Content-Type': 'application/json', 'Authorization': 'Basic eGlhby5zaGVuQGNhcGNvLmNvbTpXZWxjb21lMg==', 'Access-Control-Allow-Origin' : 'true' })
-    // };
-
-    this.http.get("/api/rest/api/2/search?jql=project+%3D+PCFQA+AND+%22Vendor+List%22+%3D+Capco+AND+%22PCF+Stream%22+%3D+%22Secure+Site:+Responsive+web%22+AND+status+not+in+(%22Ready+to+Retest%22,+Closed,+%22Capco+QA%22,+Cancelled,+Canceled,+Rejected,+Deferred,+%22Need+More+Info%22,+Duplicate,+draft,+Monitor,+Fixed)+AND+priority+in+(%221-+Urgent%22,+%222-+High%22,+%223-+Medium%22)+AND+assignee+in+(riju.vashisht,+Ricardo.Razo,+Rodrigo.Paixao,+julien.truchot,+Sreekumar.Rajan,+lucas.alba,+Xiao.Shen,+zeyu.dang,+yulisa.chang,+donny.ng,+Zheng.Fang,+Dae.Hee.Jeon)+OR+project+%3D+SSITE+AND+type+%3D+Bug+AND+assignee+in+(riju.vashisht,+Ricardo.Razo,+Rodrigo.Paixao,+julien.truchot,+Sreekumar.Rajan,+lucas.alba,+Xiao.Shen,+zeyu.dang,+yulisa.chang,+donny.ng,+Zheng.Fang,+Dae.Hee.Jeon)+AND+status+not+in+(%22Ready+to+Retest%22,+Closed,+%22Capco+QA%22,+Cancelled,+Canceled,+Rejected,+Deferred,+%22Need+More+Info%22,+Duplicate,+draft,+Monitor,+Fixed)+AND+Severity+in+(%221+-+Critical%22,+%222+-+Major%22,+%223+-+Minor%22)",
-      {
+    let filterURL="project = \"Secure Site: Responsive web\" AND type = Bug AND status not in (Closed, Deferred, Fixed, duplicate, \"Ready to Retest\", Monitor, Reject, Canceled, Cancelled, Rejected) AND assignee in (Xiao.Shen, julien.truchot, Ricardo.Razo, riju.vashisht, Rodrigo.Paixao, Zheng.Fang, donny.ng, yulisa.chang, Sreekumar.Rajan, lucas.alba) OR project = PCFQA AND type = Bug AND \"PCF Stream\" = \"Secure Site: Responsive web\" AND status not in (Closed, Deferred, Fixed, duplicate, \"Ready to Retest\", Monitor, Reject, Canceled, Cancelled, Rejected) AND assignee in (Xiao.Shen, julien.truchot, Ricardo.Razo, riju.vashisht, Rodrigo.Paixao, Zheng.Fang, donny.ng, yulisa.chang, Sreekumar.Rajan, lucas.alba) ORDER BY status DESC","viewUrl":"https://capco-cardinal.atlassian.net/issues/?filter=18342","searchUrl":"https://capco-cardinal.atlassian.net/rest/api/2/search?jql=project+%3D+%22Secure+Site:+Responsive+web%22+AND+type+%3D+Bug+AND+status+not+in+(Closed,+Deferred,+Fixed,+duplicate,+%22Ready+to+Retest%22,+Monitor,+Reject,+Canceled,+Cancelled,+Rejected)+AND+assignee+in+(Xiao.Shen,+julien.truchot,+Ricardo.Razo,+riju.vashisht,+Rodrigo.Paixao,+Zheng.Fang,+donny.ng,+yulisa.chang,+Sreekumar.Rajan,+lucas.alba)+OR+project+%3D+PCFQA+AND+type+%3D+Bug+AND+%22PCF+Stream%22+%3D+%22Secure+Site:+Responsive+web%22+AND+status+not+in+(Closed,+Deferred,+Fixed,+duplicate,+%22Ready+to+Retest%22,+Monitor,+Reject,+Canceled,+Cancelled,+Rejected)+AND+assignee+in+(Xiao.Shen,+julien.truchot,+Ricardo.Razo,+riju.vashisht,+Rodrigo.Paixao,+Zheng.Fang,+donny.ng,+yulisa.chang,+Sreekumar.Rajan,+lucas.alba)+ORDER+BY+status+DESC";
+    this.http.get("/api/rest/api/2/search?jql=" + filterURL,     {
         headers: {
           'Content-Type': 'application/json',
           'Authorization': 'Basic eGlhby5zaGVuQGNhcGNvLmNvbTpXZWxjb21lMg==',
@@ -41,8 +37,8 @@ export class AppComponent {
         }
       })
       .subscribe(data => {
+        console.log(data);
         if (!!data && !!data['issues']) {
-          // console.log(data.issues);
           for (let i = 0; i < data['issues'].length; i++) {
             var issueData = data['issues'][i];
             var issueKey = issueData.key;
@@ -51,16 +47,23 @@ export class AppComponent {
             var severity = issueData.fields.customfield_10900.value;
             var description = issueData.fields.description;
             var summary = issueData.fields.summary;
-            // console.log(data.issues[i].fields.assignee.name);
-            if (this.viewModel.hasOwnProperty(assignee)) {
-              //  already has such assignee in model
-              this.viewModel[assignee]['issueCount']++;
-              this.viewModel[assignee]['issues'].push({key: issueKey, summary: summary, description: description});
+            var status = issueData.fields.status.name;
 
-            } else {
-              this.viewModel[assignee] = {};
-              this.viewModel[assignee]['issueCount'] = 1;
-              this.viewModel[assignee]['issues'] = [{key: issueKey, summary: summary, description: description}];
+            // console.log(data.issues[i].fields.assignee.name);
+            if (status.trim() !== "Under Code Review") {
+              if (this.viewModel.hasOwnProperty(assignee)) {
+                //  already has such assignee in model
+                // this.viewModel[assignee]['issueCountNoPR']++;
+                this.viewModel[assignee]['issueCount']++;
+                this.viewModel[assignee]['issues'].push({key: issueKey, summary: summary, description: description});
+              }   else {
+                this.viewModel[assignee] = {};
+                // if (status.trim() !== "Under Code Review") {
+                // this.viewModel[assignee]['issueCountNoPR'] = 1;
+                this.viewModel[assignee]['issueCount'] = 1;
+                this.viewModel[assignee]['issues'] = [{key: issueKey, summary: summary, description: description}];
+                // }
+              }
             }
 
           }
@@ -84,7 +87,7 @@ export class AppComponent {
       })
       .subscribe(data => {
         if (!!data && !!data['values']) {
-          console.log(data['values']);
+          // console.log(data['values']);
           for (let i = 0; i < data['values'].length; i++) {
             let issueData = data['values'][i];
             let title = issueData['title'];
@@ -106,15 +109,8 @@ export class AppComponent {
           }
 
 
-
         }
       });
-
-
-
-
-
-
 
 
   }
